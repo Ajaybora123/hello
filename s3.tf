5989ae3c20095155288c2558dcc5c1d626fd01bc
Ajaybora123 <125376697+Ajaybora123@users.noreply.github.com>
2025-08-04 15:46:15 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,26 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
-    bucket_prefix = var.bucket_prefix
-    acl    = var.acl
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the bucket."
+  type        = map(string)
+  default     = {}
+}
 
     versioning {
         enabled = var.versioning
     }
+     
     tags = var.tags
+    }
+
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = var.bucket_prefix
+  acl           = var.acl
+
+  versioning {
+    enabled = var.versioning
+  }
+
+  tags = var.tags
 }
